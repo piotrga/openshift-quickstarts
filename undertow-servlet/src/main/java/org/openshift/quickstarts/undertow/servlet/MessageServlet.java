035b192925615e706a30564614ffeb8539c677be
Piotr Gabryanczyk <piotrga@gmail.com>
2017-09-07 09:46:04 +0100

    Update MessageServlet.java

--- undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/MessageServlet.java
+++ undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/MessageServlet.java
@@ -26,6 +26,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.InetAddress;
 
 /**
  * @author Stuart Douglas
